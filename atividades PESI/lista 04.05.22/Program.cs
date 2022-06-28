06d2b267da77c46afc79fdd492868876533007d8
gabrielchv <gabriel.chaves.3720@outlook.com>
2022-06-28 15:30:18 -0300

    dew 28.06.22

--- atividades PESI/lista 04.05.22/Program.cs
+++ atividades PESI/lista 04.05.22/Program.cs
@@ -1,11 +1,1 @@
-﻿Console.Write("Digite o número da sua agência: ");
-String agenciaOriginal = Console.ReadLine();
-int[] agencia = new int[agenciaOriginal.Length]; 
-    
-for (int i = 0; i < agenciaOriginal.Length; i++) { 
-    agencia[i] = int.Parse(agenciaOriginal[i].ToString()); 
-}
-
-int digitoV = 11 - ((agencia[0] * 5 + agencia[1] * 4 + agencia[2] * 3 + agencia[3] * 2) % 11);
-
-Console.Write(agenciaOriginal + "-" + digitoV);
+﻿int x = 1 < 0 ? 0 : 1;
